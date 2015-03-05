ed80ffe87df469339c4aa4daf26b34e4e56d4c20
Pu Hongwu <puhongwu@live.com>
2015-03-06 00:43:24 +0800

    join less
    使用less编写样式文件

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,8 +1,8 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html lang="zh-cn">
   <head>
-  <title>Bootstrap</title>
-  <meta charset="utf-8">
+	  <meta charset="utf-8">
+<title>Mteam</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="/Mteam/Public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -11,45 +11,58 @@
   <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
-    <style type="text/css">
-    	.starter-template {
-		    padding: 40px 15px;
-		    text-align: center;
-		}
-    </style>
+	  <link rel="stylesheet" type="text/css" href="/Mteam/Public/css/index.css"/>
   </head>
   <body>
-    <nav class="navbar navbar-inverse" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Mteam</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">首页</a></li>
-            <li><a href="#about">团队</a></li>
-            <li><a href="#contact">项目</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="<?php echo U('Home/Sign/signin');?>" tppabs="">登录</a></li>
-            <li><a href="<?php echo U('Home/Sign/signup');?>" tppabs="">注册</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
+  	  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
-      <div class="starter-template">
-        <h1>alonehover毕业设计</h1>
-        <p class="lead">基于html5的校园项目团队管理系统</p>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo U('Home/Index/index');?>">Mteam</a>
       </div>
-    </div><!-- /.container -->
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="<?php echo U('Home/Index/index');?>">首页</a></li>
+          <li><a href="#about">团队</a></li>
+          <li><a href="#contact">项目</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        <?php if(session('stunum')): ?><li><a href="#" tppabs=""><?php echo (session('stunum')); ?></a></li>
+          <li><a href="<?php echo U('Home/Sign/signout');?>" tppabs="">[退出]</a></li>
+        <?php else: ?>
+          <li><a href="<?php echo U('Home/Sign/signin');?>" tppabs="">登录</a></li>
+          <li><a href="<?php echo U('Home/Sign/signup');?>" tppabs="">注册</a></li><?php endif; ?>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
+  	<div class="masthead jumbotron">
+    	<div class="container">
+	        <h1>Mteam</h1>
+	        <p class="lead">基于html5的校园项目团队管理系统</p>
+      	</div>
+    </div><!-- /.container-top -->
+    <div class="container">
+      <div class="row text-center mt-num">
+        <div class="col-md-4">
+          <div class="num">12</div>
+          <h2>已有团队</h2>
+        </div>
+        <div class="col-md-4">
+          <div class="num">12</div>
+          <h2>注册用户</h2>
+        </div>
+        <div class="col-md-4">
+          <div class="num">12</div>
+          <h2>发布项目</h2>
+        </div>
+      </div>
+    </div>
     <script src="/Mteam/Public/js/jquery.min.js"></script>
 <script src="/Mteam/Public/bootstrap/js/bootstrap.min.js"></script>
   </body>
