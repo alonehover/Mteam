b521de357f8396a2be36ba0792b8e4a6b71310a3
Pu Hongwu <puhongwu@live.com>
2015-03-07 00:07:29 +0800

    add user admin
    建立用户管理界面

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./Application/');
 
-//define('BIND_MODULE','Admin');
+// define('BIND_MODULE','User');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
