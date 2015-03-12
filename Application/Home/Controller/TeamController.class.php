272e6ba3ec5db8413fed9c2f7d94cf2bf9e88201
Pu Hongwu <puhongwu@live.com>
2015-03-12 16:02:39 +0800

    edit page
    制作团队详情页
    修改团队列表页样式

--- Application/Home/Controller/TeamController.class.php
+++ Application/Home/Controller/TeamController.class.php
@@ -4,13 +4,14 @@ namespace Home\Controller;
 use Think\Controller;
 class TeamController extends Controller {
 
-	public function index()
-	{
+	public function index(){
 		# code...
 		$this->assign('active','team');
 		$this->display();
 	}
 
-
+	public function team_info(){
+		$this->display();
+	}
 }
  ?>
