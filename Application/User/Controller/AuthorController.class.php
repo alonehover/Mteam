2693ac8533339dd4808e7a2af3724a1644c255a0
Pu Hongwu <puhongwu@live.com>
2015-03-20 17:23:58 +0800

    用户添加

--- Application/User/Controller/AuthorController.class.php
+++ Application/User/Controller/AuthorController.class.php
@@ -12,6 +12,46 @@ class AuthorController extends Controller {
 
     public function add_user(){
     	# code...
-    	$this->display();
+        if (IS_POST) {
+            # code...
+            $data = array(
+                'stunum' => I('stunum'),
+                'authorname' => I('username'),
+                'password' => MD5(I('password')),
+                'sex' => I('sex'),
+                'email' => I('email'),
+                'skill' => implode(',',(array)I('skill')),
+                'info'  => I('info'),
+                'logintime' => time(),
+             );
+            $flag=M('author')->add($data);
+            if ($flag) {
+                # code...
+                $this->success('添加成功！',U('User/Author/list_user'));
+            }else{
+                $this->error('添加失败！');
+            }
+        }else{
+            $skill=M('skill')->select();
+            $this->assign('skill',$skill);
+            $this->display();
+        }
+        
+    }
+
+
+    public function del_user(){
+        # code...
+        if (IS_POST) {
+            # code...
+            $data['id']=I('id');
+            $flag=M('author')->where($data)->delete();
+            if ($flag) {
+                # code...
+                echo json_encode(1);
+            }else{
+                echo json_encode(0);
+            }
+        }
     }
 }
