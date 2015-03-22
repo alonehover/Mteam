9a804d595a2905a4aadde1b2bfe4f718f36c4313
Pu Hongwu <puhongwu@live.com>
2015-03-22 22:50:47 +0800

    添加项目列表页

---
+++ Application/User/Controller/ProjectController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace User\Controller;
+use Think\Controller;
+class ProjectController extends Controller {
+    public function list_project(){
+    	$this->display();
+    }
+
+    public function list_project_apply(){
+    	# code...
+    	$this->display('list_apply');
+    }
+}
