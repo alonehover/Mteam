fa15e13d4a844908c3816daee3a1ec8467600b37
Pu Hongwu <puhongwu@live.com>
2015-03-25 00:09:08 +0800

    项目发布功能实现

--- Application/User/Controller/ProjectController.class.php
+++ Application/User/Controller/ProjectController.class.php
@@ -3,6 +3,8 @@ namespace User\Controller;
 use Think\Controller;
 class ProjectController extends Controller {
     public function list_project(){
+        $data=M('project')->select();
+        $this->assign('project',$data);
     	$this->display();
     }
 
@@ -13,6 +15,31 @@ class ProjectController extends Controller {
 
     public function add_project(){
     	# code...
-    	$this->display();
+        if (IS_POST) {
+           $data = array(
+                'name'=> I('project_name'),
+                'skill'=>implode(',',(array)I('skill')),
+                'apply_endtime' => strtotime(I('apply_end')),
+                'project_endtime' => strtotime(I('project_end')),
+                'status' => I('status'),
+                'desc' => I('project_desc'),
+                'public_time' => time(),
+            );
+            $flag=M('project')->add($data);
+            if ($flag) {
+                # code...
+                $this->success('添加成功！',U('User/Project/list_project'));
+
+            }else{
+                $this->error('添加失败！');
+            }
+        }else{
+            $skill=M('skill')->select();
+            $this->assign('skill',$skill);
+            $this->display();
+        }
+        
     }
+
+
 }
